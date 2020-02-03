21d03fb9235e894eb50d8f551f07f4587dca04eb
IsuruMS <50530525+IsuruMS@users.noreply.github.com>
2020-02-03 19:41:24 +0530

    Add files via upload

---
+++ Algorithm1.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<time.h>
+#include<stdlib.h>
+
+long insideCircle = 0;      // number of points inside the circlr 
+long totalPoints = 0;      // number of points generated
+int pointsPerThread;        //number of points to be created in a thread
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void *Algorithm()   //algorithm to generate random points and calculate how many are inside 
+{
+
+    long insideCirclrInThread = 0;   
+    long pointsInThread = 0;
+    unsigned int points = rand();       //random numbers generator
+
+    for(int i=0; i<pointsPerThread; i++)
+    {
+        double xCord = rand_r(&points) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+        double yCord = rand_r(&points) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
+        pointsInThread++;
+
+        //checking for which of the above points are inside the circle with the radius 1 units
+        if(xCord*xCord + yCord*yCord <1)
+        {
+            insideCirclrInThread++;
+        }
+    }
+    //mutex lock to prevent race conditions
+    pthread_mutex_lock(&lock);
+    insideCircle += insideCirclrInThread;
+    totalPoints += pointsInThread;
+    pthread_mutex_unlock(&lock);
+}
+
+//main fucntion gets two inputs
+//number of threads and number of points in the begining
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "usage: ./<compiled_name> <initial no. of threads> <initial no. of points>\n");
+        exit(1);
+    }
+
+    int threadsNum = atoi(argv[1]);
+    long initTotalPoints = atol(argv[2]);
+
+    //files to write the outputs
+    FILE *outFile;
+    outFile = fopen("results1.txt" , "w+");
+    FILE *outFile1;
+    outFile1 = fopen("TotalPoints1.txt" , "w+");
+
+        fprintf(outFile,"Number of Threads = %d\n\n", threadsNum);
+        for(int j=0; j<10000; j++)       //incrementing the number of made points
+        {        
+            insideCircle = 0;
+            totalPoints = 0;
+
+            pointsPerThread = initTotalPoints/threadsNum;
+
+            time_t start = clock();
+
+            pthread_t *threads = malloc(threadsNum * sizeof(pthread_t));
+
+            pthread_attr_t attribute;
+            pthread_attr_init(&attribute);
+
+            for(int k=0; k < threadsNum; k++)
+            {
+                pthread_create(&threads[k], &attribute, Algorithm, (void *) NULL);
+            }
+
+            for(int k=0; k < threadsNum; k++)
+            {
+                pthread_join(threads[k], NULL);
+            }
+            time_t end = clock();
+            pthread_mutex_destroy(&lock);
+            free(threads);      //free the dynamic space allocated for threads
+
+            //Pi estimation
+            long double pi = 4 * (long double)insideCircle / (long double)totalPoints;
+            float timeTaken = ((end - start)/CLOCKS_PER_SEC) * 1000;
+            
+            //output
+            fprintf(outFile,"%Lf\n", pi);
+            fprintf(outFile1,"%ld\n", totalPoints);
+
+            initTotalPoints += 100;
+        }
+    printf("Done. Check the files results1.txt and TotalPoints1.txt made in the directory.\n");
+}
